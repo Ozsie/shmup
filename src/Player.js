85bde5f401e3e5f83b9c031b789af13119499ef9
Oscar Djupfeldt <oscar.djupfeldt@abilia.se>
2025-03-21 10:15:18 +0100

    player can't leave canvas

--- src/Player.js
+++ src/Player.js
@@ -10,11 +10,22 @@ export class Player {
     this.sprite = new Image();
   }
 
-  update() {
+  update(canvas) {
     if (keys['ArrowUp']) this.y -= this.speed;
     if (keys['ArrowDown']) this.y += this.speed;
     if (keys['ArrowLeft']) this.x -= this.speed;
     if (keys['ArrowRight']) this.x += this.speed;
+    if (this.x < 0) {
+      this.x = 0;
+    } else if (this.x + this.width > canvas.width) {
+      this.x = canvas.width - this.width;
+    }
+
+    if (this.y < 0) {
+      this.y = 0;
+    } else if (this.y + this.height > canvas.height) {
+      this.y = canvas.height - this.height;
+    }
   }
 
   draw(ctx) {

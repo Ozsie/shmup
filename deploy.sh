54950193106115273f78e5df0071c359910a8a95
Oscar Djupfeldt <oscar.djupfeldt@abilia.se>
2025-03-25 08:04:32 +0100

    clean up deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status
 set -e
 
-# Define the temporary directory
-TEMP_DIR=$(mktemp -d)
+mkdir dist
+cp -r src/* dist
 
-# Checkout the main branch and copy the src directory to the temporary location
-cp -r src/* "$TEMP_DIR"
-
-# Checkout the gh-pages branch
 git checkout gh-pages
 git pull
 
-# Copy the contents of the src directory from the temporary location to the root directory
-cp -r "$TEMP_DIR"/* .
+cp -r dist/* .
 
-# Add, commit, and push the changes
 git add .
 git commit -m "Deploy src to gh-pages"
 git push origin gh-pages
 
-# Clean up the temporary directory
-rm -rf "$TEMP_DIR"
-
-# Checkout back to the main branch
+rm -rf dist
 git checkout main

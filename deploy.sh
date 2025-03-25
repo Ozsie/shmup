7a88bf9661100aab917169104d779a50c4d2c120
Oscar Djupfeldt <oscar.djupfeldt@abilia.se>
2025-03-25 16:36:36 +0100

    use dist_config when deploying

--- deploy.sh
+++ deploy.sh
@@ -4,21 +4,17 @@ set -e
 
 mkdir dist
 cp -r src/* dist
+rm dist/config.js
+mv dist/dist_config.js dist/config.js
 
 git checkout gh-pages
 git pull
 
 cp -r dist/* .
 
-echo 'export const config = {
-        "hitboxes": false,
-        "grid": false,
-        "death": false,
-      }' > dist/config.js
-
 git add .
 git commit -m "Deploy src to gh-pages"
 git push origin gh-pages
 
-#rm -rf dist
+rm -rf dist
 git checkout main
---
+++ src/dist_config.js
@@ -0,0 +1,5 @@
+export const config = {
+  "hitboxes": false,
+  "grid": false,
+  "death": true,
+}

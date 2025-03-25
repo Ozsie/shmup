c39ea9045fa16860fa0b9e44ba6165c6b3ffd33d
Oscar Djupfeldt <oscar.djupfeldt@abilia.se>
2025-03-25 16:31:55 +0100

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -10,9 +10,15 @@ git pull
 
 cp -r dist/* .
 
+echo 'export const config = {
+        "hitboxes": false,
+        "grid": false,
+        "death": false,
+      }' > dist/config.js
+
 git add .
 git commit -m "Deploy src to gh-pages"
 git push origin gh-pages
 
-rm -rf dist
+#rm -rf dist
 git checkout main
